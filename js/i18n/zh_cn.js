3caaf967836bad0006d6b1d4215a354b79cc5828
zzllrr小乐笑了 <767635+zzllrr@users.noreply.github.com>
2019-04-12 09:32:50 +0800

    Add files via upload

---
+++ js/i18n/zh_cn.js
@@ -0,0 +1,546 @@
+/*
+ * zzllrr Mather
+ * Copyright by zzllrr since 2013. All rights reserved.
+ * zzllrr@gmail
+ * Released under MIT License
+ */
+if(!lang){var lang={}}
+lang['zh_cn']={
+	
+
+//non-standard key for Interface
+
+	'sbson':'字符',
+	'funcson':'函数',
+	'strucon':'结构',
+	'Condon':'条件',
+	'thTip':'点击表头，可以显示/编辑LaTeX',
+	'sbsonTip':'同时按住Shift键，则得到Unicode字符，而不是LaTeX格式',
+	'funcsonTip':"同时按住Shift键，则函数后面不自动加(括号)",
+	'struconTip':"同时按住Shift键，则得到$内部函数命令$，而不是LaTeX格式",
+	'taskTip':"同时按住Shift键，则自动输入示例命令",
+	'donaTip':'需要您的支持！',
+	'donaTip0':'千里之行，始于足下<br />万年之兴，基于数学',
+	'Accnum':'收款账号',
+	'copiedTip':'已粘贴到剪贴板',
+	'Five-year research plan':'未来五年主要工作计划',
+	'WeChat':'微信公众号',
+	'Contact':'联系',
+	'International Standard Open-Sourced Math Tool Program by Experts':'国际通用标准的专家级开源数学工具项目',
+	'Customized Math Tool Program based on Mather by zzllrr':'小乐基于Mather个性化开发的数学工具项目',
+	'Customized Math Tool Program based on Mather by someone':'某人基于Mather个性化开发的数学工具项目',
+// standard 
+
+//Tool
+	'Solve':'解题',
+	'Graphic':'作图',
+	'Show':'演示',
+	'Explore':'探索',
+	'About':'关于',
+	
+	'Wiki':'百科',
+	'Drill':'题库',
+	'Course':'课程',
+	'Topic':'课题',
+	'Pitfall':'陷阱',
+	'Unsolved':'未解',
+	'Refer':'参考',
+
+	'Input':'输入',
+	'Output':'输出',
+	'Voice':'语音',
+	'Video':'视频',
+	'Record':'录制',
+	'Mic':'麦克风',
+	'Camera':'摄像头',
+	'Default':'默认',
+
+	'input your words':'输入你想说的话',
+	'input your Audio/Video URL':'输入你的音视频地址',
+	'Engine':'引擎',
+	'Person':'人物',
+	'Speed':'速度',
+	'Pitch':'音调',
+	'Language':'语言',
+	'Lang':'语言',
+
+	'Text':'文本',
+
+	'Font':'字体',
+	'Size':'大小',
+	'Color':'颜色',
+	'Margin':'间距',
+	'Phantom':'隐形',
+
+	'Shape':'形状',
+	'Pie':'饼图',
+	'Bar':'条形图',
+	'Polyline':'折线图',
+	'Project':'工程',
+	'Flowchart':'流程图',
+	'Statistics':'统计',
+
+	'lineSplit':'分行',
+	'lineMerge':'合行',
+
+	'Edit':'编辑',
+	'Replace':'替换',
+	'Copy':'复制',
+	'Delete':'删除',
+	'Clear':'清除',
+	'Refresh':'刷新',
+
+	'Display':'显示',
+	'Play':'播放',
+	'Pause':'暂停',
+	'Stop':'停止',
+
+	'FullScreen':'全屏',
+		
+	'zzllrr Mather':'小乐数学',
+	'Mather':'数学者',
+	
+	'Contact':'联系',
+	'Version':'版本',
+	'Whitepaper':'白皮书',
+	'Donate':'捐赠',
+	'Contributor':'贡献者',
+
+	'Payee':'收款人',
+	'Summary':'简介',
+
+	'Name':'名称',
+	'Aims':'宗旨',
+	'Vision':'愿景',
+	'Protocol':'协议',
+	'Organism':'有机体',
+	'Offline':'离线',
+	'Computation':'计算',
+	'Online Connection':'联网',
+	'Energy Consumption Minimization':'能耗最小化',
+	'Open Access':'开放获取',
+	'Consensus':'共识',
+	'Cooperation':'合作',
+	'Localized':'本地化',
+	'Program':'项目',
+	'Description':'描述',
+	
+	'3rd-party':'第三方',
+	'Library':'库',
+	'File':'文件',
+	
+	'Search':'搜索',
+
+// Math operation
+	'Unary':'一元',
+	'Binary':'二元',
+	'Ternary':'三元',
+	'Multiary':'多元',//Multiary n-ary Polyadic
+	'Operation':'运算',
+
+	'Elementary':'初等',
+	'Parameter':'参数',
+	
+	'Type':'类型',
+	'Structure':'结构',
+
+
+// Math obj
+	'Number':'数',
+	'Integer':'整数',
+	'Positive':'正',
+	'Natural':'自然数',
+	
+	'Fraction':'分数',
+	
+	'Decimal':'小数',
+	'Rational':'有理数',
+	'Irrational':'无理数',
+	'Transcendental':'超越数',
+	'Real':'实数',
+	'Imaginery':'虚数',
+	'Complex':'复数',
+
+	'Prime':'质数',
+
+	'Sequence':'数列',
+
+	
+	'Polynomial':'多项式',
+
+	'Matrix':'矩阵',
+	
+	'Permutation':'置换',
+
+	'Relation':'关系',
+
+	'Function':'函数',
+
+
+	'Proposition':'命题',
+
+	'Set':'集合',
+
+
+
+//of Math subject
+	'Arithmetic':'算术',
+	
+	'Algebraic':'@3',
+
+	'Geometric':'几何',
+
+	'Trigonometric':'三角',
+	
+	'Hyperbolic':'双曲',
+
+	'Analytical':'分析',
+
+	'Logistical':'逻辑',
+
+	'Probabilistic':'概率',
+
+	'Statistical':'统计',
+
+	'Topological':'拓扑',
+
+
+
+//Math concept
+	'Divide':'整除',
+	'Factorization':'因数分解',
+	'Greatest Common Divisor':'最大公约数',
+	'Least Common Multiple':'最小公倍数',
+	'Matrix Factorization':'矩阵分解',
+	'Eigen':'特征',
+	'Value':'值',
+	'Block':'分块',
+	'Transpose':'转置',
+	'Rank':'秩',
+	'Trace':'迹',
+	'Row':'行',
+	'Column':'列',
+	'Adjoint':'伴随',
+	'Augmented':'增广',
+	'Rule':'法则',
+	'Order':'阶',
+	
+	'Coprime':'互素',
+	'Factorial':'阶乘',
+	'Digit':'数位',
+	'Unit':'单位',
+	
+	'Sum':'求和',
+	'Prod':'求积',
+	'Combination':'组合',
+
+	'Fermat':'费马',
+	
+	'Prime Mersenne Number':'梅森素数',
+	'Amicable Numbers':'亲和数',
+	
+	
+	'Recursive':'递归',
+
+	'Enumerate':'枚举',
+	'Determinant':'行列式',
+	'Norm':'模',
+	'Normalize':'单位化',
+	'Orthogonalize':'正交化',
+	'Gram-Schmidt':'正交单位化',
+	'Inverse':'逆',
+	'Transpose':'转置',
+	'Hermitian':'共轭转置',
+	'Diagonalize':'对角化',
+	'Diagonal':'对角',
+	'Expansion':'展开',
+	'Symmetric':'对称',
+	'Anti':'反',
+	'Identity Matrix':'单位矩阵',
+	'Square Matrix':'方阵',
+
+	'Expression':'表达式',
+	'Classification':'分类',
+	'By':'按',
+	'Definition':'按定义',
+	'Inner':'内',
+	'Outer':'外',
+	'Product':'积',
+	'Cross':'叉',
+	
+	'Division':'除法',
+	
+	'Quadratic':'二次',
+	'Quadric':'二次',
+	'Form':'型',
+
+	'Cofactor':'余子式',
+	'Singular':'奇异',
+	
+	'Inversion':'逆序',
+	'Minor':'子式',
+	'Base':'基',
+	'Space':'空间',
+	'Linear':'线性',
+	
+	'Equation':'方程',
+	'Equation Array':'方程组',
+	
+	'Inequation':'不等式',
+	'Analytic':'解析式',
+	
+	'Equivalence':'等价关系',
+	'Partial Order':'偏序',
+	
+	'Dot':'点',
+	'Boolean':'布尔',
+	
+	'Vector':'向量',
+	'Tensor':'张量',
+	'Limit':'极限',
+	'Power':'幂',
+	'Radical':'根式',
+	
+	'Series':'级数',
+	'Infinite':'无限',
+	'Formal':'形式',
+	'Differential':'微分',
+	'Integral':'积分',
+	'Logic':'逻辑',
+	'Algebra':'代数',
+	'Geometry':'几何',
+	'Graph':'图',
+
+
+	'Point':'点',
+	'Line':'线',
+	'Angle':'角',
+	'Plane':'平面',
+	'Solid':'立体',
+
+	'Path':'路径',
+
+
+	'Group':'群',
+	'Ring':'环',
+	'Lattice':'格',
+
+
+
+
+	'Convert':'转换',
+	'Express':'表示',
+	'Simplify':'化简',
+	'Calculate':'计算',
+	'Prove':'证明',
+
+	'Cover':'覆盖',
+	'Chain':'链',
+
+	'Transform':'变形',
+	
+	'Transformation':'变换',
+	'Translate':'平移',
+	'Condition':'条件',
+
+
+
+//Math Topic
+
+	'Lottery':'彩票',
+	
+	'Sudoku':'数独',
+
+
+
+//Math subject
+
+	'Math':'@0',
+	'Mathematics':'@0',
+	"Mathematical":'@0',
+
+	"0":"数学",
+	"1":"@0",
+	"2":"@0",
+	"3":"代数",
+
+	'Subject Classification GB':'中国国家标准数学学科分类',
+
+	"11":"数学史",
+	"14":"数理逻辑与数学基础",
+	"1410":"演绎逻辑学",
+	"1420":"证明论",
+	"1430":"递归论",
+	"1440":"模型论",
+	"1450":"公理集合论",
+	"1460":"数学基础",
+	"1499":"@99",
+	"17":"数论",
+	"1710":"初等数论",
+	"1720":"解析数论",
+	"1730":"代数数论",
+	"1740":"超越数论",
+	"1750":"丢番图逼近",
+	"1760":"数的几何",
+	"1770":"概率数论",
+	"1780":"计算数论",
+	"1799":"@99",
+	"21":"代数学",
+	"2110":"线性代数",
+	"2115":"群论",
+	"2120":"域论",
+	"2125":"李群",
+	"2130":"李代数",
+	"2135":"Kac-Moody代数",
+	"2140":"环论",
+	"2145":"模论",
+	"2150":"格论",
+	"2155":"泛代数理论",
+	"2160":"范畴论",
+	"2165":"同调代数",
+	"2170":"代数K理论",
+	"2175":"微分代数",
+	"2180":"代数编码理论",
+	"2199":"@99",
+	"24":"代数几何学",
+	"27":"几何学",
+	"2710":"几何学基础",
+	"2715":"欧氏几何学",
+	"2720":"非欧几何学",
+	"2725":"球面几何学",
+	"2730":"向量和张量分析",
+	"2735":"仿射几何学",
+	"2740":"射影几何学",
+	"2745":"微分几何学",
+	"2750":"分数维几何",
+	"2755":"计算几何学",
+	"2799":"@99",
+	"31":"拓扑学",
+	"3110":"点集拓扑学",
+	"3115":"代数拓扑学",
+	"3120":"同伦论",
+	"3125":"低维拓扑学",
+	"3130":"同调论",
+	"3135":"维数论",
+	"3140":"格上拓扑学",
+	"3145":"纤维丛论",
+	"3150":"几何拓扑学",
+	"3155":"奇点理论",
+	"3160":"微分拓扑学",
+	"3199":"@99",
+	"34":"数学分析",
+	"3410":"微分学",
+	"3420":"积分学",
+	"3430":"级数论",
+	"3499":"@99",
+	"37":"非标准分析",
+	"41":"函数论",
+	"4110":"实变函数论",
+	"4120":"单复变函数论",
+	"4130":"多复变函数论",
+	"4140":"函数逼近论",
+	"4150":"调和分析",
+	"4160":"复流形",
+	"4170":"特殊函数论",
+	"4199":"@99",
+	"44":"常微分方程",
+	"4410":"定性理论",
+	"4420":"稳定性理论",
+	"4430":"解析理论",
+	"4499":"@99",
+	"47":"偏微分方程",
+	"4710":"椭圆型偏微分方程",
+	"4720":"双曲型偏微分方程",
+	"4730":"抛物型偏微分方程",
+	"4740":"非线性偏微分方程",
+	"4799":"@99",
+	"51":"动力系统",
+	"5110":"微分动力系统",
+	"5120":"拓扑动力系统",
+	"5130":"复动力系统",
+	"5199":"@99",
+	"54":"积分方程",
+	"57":"泛函分析",
+	"5710":"线性算子理论",
+	"5715":"变分法",
+	"5720":"拓扑线性空间",
+	"5725":"希尔伯特空间",
+	"5730":"函数空间",
+	"5735":"巴拿赫空间",
+	"5740":"算子代数",
+	"5745":"测度与积分",
+	"5750":"广义函数论",
+	"5755":"非线性泛函分析",
+	"5799":"@99",
+	"61":"计算数学",
+	"6110":"插值法与逼近论",
+	"6120":"常微分方程数值解",
+	"6130":"偏微分方程数值解",
+	"6140":"积分方程数值解",
+	"6150":"数值代数",
+	"6160":"连续问题离散化方法",
+	"6170":"随机数值实验",
+	"6180":"误差分析",
+	"6199":"@99",
+	"64":"概率论",
+	"6410":"几何概率",
+	"6420":"概率分布",
+	"6430":"极限理论",
+	"6440":"随机过程",
+	"6450":"马尔可夫过程",
+	"6460":"随机分析",
+	"6470":"鞅论",
+	"6480":"应用概率论",
+	"6499":"@99",
+	"67":"数理统计学",
+	"6710":"抽样理论",
+	"6715":"假设检验",
+	"6720":"非参数统计",
+	"6725":"方差分析",
+	"6730":"相关回归分析",
+	"6735":"统计推断",
+	"6740":"贝叶斯统计",
+	"6745":"试验设计",
+	"6750":"多元分析",
+	"6755":"统计判决理论",
+	"6760":"时间序列分析",
+	"6799":"@99",
+	"71":"应用统计数学",
+	"7110":"统计质量控制",
+	"7120":"可靠性数学",
+	"7130":"保险数学",
+	"7140":"统计模拟",
+	"7199":"@99",
+	"74":"运筹学",
+	"7410":"线性规划",
+	"7415":"非线性规划",
+	"7420":"动态规划",
+	"7425":"组合最优化",
+	"7430":"参数规划",
+	"7435":"整数规划",
+	"7440":"随机规划",
+	"7445":"排队论",
+	"7450":"对策论",
+	"7455":"库存论",
+	"7460":"决策论",
+	"7465":"搜索论",
+	"7470":"图论",
+	"7475":"统筹论",
+	"7480":"最优化",
+	"7499":"@99",
+	"77":"组合数学",
+	"81":"离散数学",
+	"84":"模糊数学",
+	"87":"应用数学",
+	"99":"其它学科",
+		
+	
+	'SummaryTip':'小乐数学zzllrr Mather，是小乐原创开源实现的一款跨平台可离线运行并满足多用户背景、多场景需要的数学软件。<br />'+
+		'目前这是一个Demo版本（雏形演示），功能上只实现了数学公式显示，初等代数、线性代数、离散数学等学科的解题，<br />'+
+		'因个人能力和精力有限，缺陷和错误难以避免（如有浏览器假死情况，直接关闭网页即可）<br />'+
+		'在正式版本发布之前，内核、界面、功能可能还需经历数次重构和迭代，因此一切仅供参考。',
+	'VisionTip':'推动更多的数学、科技、教育、工程、人文工作者、爱好者加入全球开源工具项目：Mather（暂译为数学者），更专业化地普及、研究、发展数学',
+	'AimsTip':'让数学更易学易练，易教易研，易赏易玩，易见易得，易传易及。',
+
+};
+
