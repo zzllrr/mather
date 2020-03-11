24d8205e0b2abbfeffad97e05d19b1bc46e27917
xiaolezzllrr <xiaolezzllrr@xiaolezzllrrdeMacBook-Air.local>
2020-03-12 07:59:11 +0800

    improve Euler history

--- js/htm/teaching/course/history_of_mathematics.js
+++ js/htm/teaching/course/history_of_mathematics.js
@@ -16,13 +16,15 @@ Table([i18(ZLR('Name Nationality Time Image Summary Reference'))],[
 
 			['1707–1727','早年，瑞士巴塞尔Basel'],
 			[1707,'作为长子出生，父亲牧师（出身普通工匠家庭，在大学的前两年也上过数学家Jakob Bernoulli的课），母亲是学者的后代'],
-			[1708,'1岁，搬家到巴塞尔郊区；妹妹Anna Maria出生，后来Maria Magdalena、Johann Heinrich出生'],
+			[1708,'1岁，搬家到巴塞尔郊区；两个妹妹Anna Maria、Maria Magdalena，1个弟弟Johann Heinrich相继出生'],
 
-			[1715,'8岁，上拉丁学校（之前爸爸在家教他数学），住在外婆家的一个房间；父亲为弥补学校低质量的教育，雇佣了一个私教Johannes Burckhardt（神学家，但也对数学有热情）'],
+			[1711,'4岁，欢乐，充满好奇心，一次躲在鸡窝里想自己孵鸡蛋，让妈妈担心他掉到井里，四处找他；帮妈妈摘樱桃前，想一个个尝甜不甜'],
+
+			[1714,'7岁，步行一小时到巴塞尔上拉丁学校（之前爸爸在家教他数学），住在外婆家的一个房间；父亲为弥补学校低质量的教育，雇佣了Johann Bernoulli的一个年轻学生Johannes Burckhardt（神学家，但也对数学有热情）当家教'],
 
 			[1720,'13岁，进入巴塞尔大学，读哲学系，上Johann Bernoulli（1667-1748，Jakob弟弟，此时Jakob已逝）的基础数学课程，受到赏识和鼓励，并在每周六下午老师家获得指导'],
 
-			[1723,'16岁，硕士毕业，毕业演讲主题是：比较笛卡尔与牛顿的自然哲学；后来随父母意愿进入神学系，但大部分时间都花在数学上'],
+			[1723,'16岁，硕士毕业，毕业演讲主题是：比较笛卡尔与牛顿的自然哲学；后来随父母意愿进入神学系，但大部分时间都花在数学上；学生时代，欧拉能记住100以内素数的6次幂'],
 
 			[1726,'19岁，参与巴黎科学院举办的竞赛：优化船上桅杆位置'],
 
@@ -49,6 +51,7 @@ Table([i18(ZLR('Name Nationality Time Image Summary Reference'))],[
 		],'TBrc'),
 		Table([i18(ZLR('Title Author Time'))],[
 			[href(Hs+'www.cs.purdue.edu/homes/wxg/EulerLect.pdf','Leonhard Euler: His Life, the Man, and His Works'),'Walter Gautschi','2008'],
+			[href(H+'www.doc88.com/p-0691743387056.html','Leonhard Euler and the Bernoullis - Mathematicians from Basel'),'',''],
 			[href(H+'www.qulishi.com/article/201903/326811.html','趣历史：莱昂哈德·欧拉是谁 他所获的评价是什么样的'),],
 			[href(Hs+'www.360kuai.com/pc/9f8df20b0ead6190e','影响世界历史的100位科学家（19）-欧拉 数学史上四杰之一'),]
 		],'TBrc'),
