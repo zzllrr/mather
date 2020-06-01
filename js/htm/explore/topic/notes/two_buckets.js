521f7d910bb97b879fdcec3209a0122534d1328e
zzllrr小乐笑了 <767635+zzllrr@users.noreply.github.com>
2020-06-01 17:32:34 +0800

    improve 2-bucket problem

--- js/htm/explore/topic/notes/two_buckets.js
+++ js/htm/explore/topic/notes/two_buckets.js
@@ -8,7 +8,7 @@ explore['Topic/Short Notes/Two Buckets'] =
 
 detail('两水桶问题f(x,y,z)',
 	[	
-		'大水桶容积 x='+num(5)+'L，小水桶容积 y='+num(3)+'L，如何得到'+num(1)+'L的水'+btnGo+
+		'大水桶容积 x='+num(5,1)+' L，小水桶容积 y='+num(3,1)+' L，如何得到'+num(1,1)+' L的水'+btnGo+
 		DCtv('ans" data-q="2-buckets')
 	].join(br)+
 
@@ -53,17 +53,17 @@ $(function(){
 						tArr.push('M',d+wn*i/2, parseInt(h-i*hn)+Ihn ,'H',d+wn*i/2+c*wn);
 						
 
-						txtArr.push(svgf.text(i, [parseInt(h-i*hn)+Ihn+d*3, wn*i/2+c*wn+d*10, 16]));
+						txtArr.push(svgf.text(i, [parseInt(h-i*hn)+Ihn+d*3, wn*i/2+c*wn+d*10, 16],'green'));
 
 						if(i){
 
-							txtArr.push(svgf.text(i, [parseInt(h-i*hn)+Ihn+d*3, wn*i/2-d*10, 16]));
+							txtArr.push(svgf.text(i, [parseInt(h-i*hn)+Ihn+d*3, wn*i/2-d*10, 16],'green'));
 							
 						}
 					
 					}
-					txtArr.push(svgf.text(r, [parseInt(h-r*hn)+Ihn+d*3, wn*r/2+c*wn+d*10, 16]));
-					txtArr.push(svgf.text(r, [parseInt(h-r*hn)+Ihn+d*3, wn*r/2-d*10, 16]));
+					txtArr.push(svgf.text(r, [parseInt(h-r*hn)+Ihn+d*3, wn*r/2+c*wn+d*10, 16],'green'));
+					txtArr.push(svgf.text(r, [parseInt(h-r*hn)+Ihn+d*3, wn*r/2-d*10, 16],'green'));
 
 
 
@@ -88,10 +88,89 @@ $(function(){
 						txtArr.push(svgf.text(c-j, [Ihn-d*5, parseInt(w+d-wn*j+wn/2), 16], "red"));
 
 					}
+					//4条边各一个点，每个点有两个方向，最终到达顶点（左上或右下，即只有一个桶装满水，全空，全满不考虑），若折线数最少，则步骤最少。
+					var ansA=[];
+					if(q==l1 || q==l2){
+						ansA.push('只需一步（'+'小大'[+(q==l2)]+'桶装满水），即可')
+					}else{
+						if(q<l1){
+							var A2=[[[q,l1],[q+l1,0]], [[l2,q],[l2+q-l1,l1]]];
+
+						}else{
+
+							var A2=[[[q,l1],[l2,q+l1-l2]], [[l2-l1,l1],[l2-l1,0]]];
+						}
+						var f=function(A){
+							var last=A.slice(-1)[0], lastt=A.slice(-2)[0];
+							//console.log(lastt, last);
+							if(last[0] == l2 && last[1]==0 || last[0] == 0 && last[1]==l1  ){// 左上或右下，即只有一个桶装满水
+								A.push([0,0])
+							}else{
+								if(lastt[0]+lastt[1]==last[0] + last[1] ){// 交换 （左上，然后水平向右 或左下；右下，然后右上 或水平向左）	//lastt[0]==last[1] && lastt[1]==last[0] 
+									if(last[0]==0){
+										if(lastt[0]<l1){
+											A.push([l2 ,last[1]])
+										}else{
+											A.push([last[0], 0])
+										}
+										
+
+									}else{
+										if(lastt[0]<l1){
+											A.push([last[0], l1])
+										}else{
+											A.push([0, last[1]])
+										}
+									}
+
+								}else if(lastt[1]==last[1]){// 水平，装满或倾倒大桶（左，然后右下；右，然后左上）
+									if(last[0]==0){
+										A.push([last[1], 0])
+									}else{
+										A.push([last[0]+last[1]-l1, l1])
+									}
+								}else{// 装满或倾倒小桶（左下，然后左上；右上，然后右下）
+
+									if(last[1]==0){
+										if(lastt[0]<l1){
+											A.push([0,last[0]])
+										}else{
+											A.push([last[0]+last[1]-l1,l1])
+										}
+										
+									}else{
+
+										if(lastt[0]<l1){
+											A.push([last[0]+last[1],0])
+										}else{
+											A.push([l2,last[0]+last[1]-l2])
+										}
+									}
+
+								}
+
+								
+								f(A);
+
+							}
+						};
+						Arrf(f,A2);
+						var steps=[];
+						ansA.push(Arrf(function(x){
+							if(steps.length <1 || steps[0][0]> x.length-1){
+								steps=[[x.length-1, [].concat(x)]];
+							}else if(steps.length && steps[0][0]== x.length-1){
+								steps.push([x.length-1, [].concat(x)]);
+
+							}
+							return x.reverse().join(' → ') + '（'+(x.length-1)+'步）'},A2).join(br));
+
+						ansA.push('最短步数是'+steps[0][0]);
+					}
 
 					ans.html(svgf.id('buckets',svgf.path(tArr.join(' '),RandomColor()+'" stroke-linejoin="round')+
 						txtArr.join(''),
-					1,'2" width="'+(w+wn+d*3)+'" height="'+parseInt(h+hn*2+d*3)))
+					1,'2" width="'+(w+wn+d*3)+'" height="'+parseInt(h+hn*2+d*3))+br+ansA.join(br))
 
 
 				}
