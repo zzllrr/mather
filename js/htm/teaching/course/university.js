7bd80765c2dc0aa789a4bfa3f06e65cae207dc03
zzllrr小乐笑了 <767635+zzllrr@users.noreply.github.com>
2021-04-19 15:44:09 +0800

    add useful online source

--- js/htm/teaching/course/university.js
+++ js/htm/teaching/course/university.js
@@ -6,6 +6,7 @@
 
 teaching['Course/University']=Table([i18(ZLR('Name Type Summary'))],[
 	[href(H+'cc.jlu.edu.cn/G2S/ShowSystem/CourseList.aspx?OrgID=90','吉林大学数学学院'),gM('Web'),''],
+	[[href(Hs+'ebook.hep.com.cn/ebooks/index.html','高教书苑'),'',''],],
 
 
 ],'TBrc');
