03a3bb9ca026e750de27af7cd5d7efa929e462f8
zzllrr小乐笑了 <767635+zzllrr@users.noreply.github.com>
2022-04-10 21:53:09 +0800

    2022.4.10

--- js/htm/index/navhead/about.js
+++ js/htm/index/navhead/about.js
@@ -19,7 +19,7 @@ $(function(){
 
 			//version
 
-			fieldseth('V2021.8.28',
+			fieldseth('V2022.4.10',
 
 				DCtv('alignc',
 					DCtv('floatl','<h1 class=logo data-z data-z-event="pointer">zzllrr Mather</h1>'+
